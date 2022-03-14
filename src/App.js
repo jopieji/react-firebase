cefc93fc42231898901faa0a5adcca467bddf772
Jake Opie <72716605+jopieji@users.noreply.github.com>
2022-03-14 15:21:19 -0500

    hosting set up

--- src/App.js
+++ src/App.js
@@ -7,6 +7,22 @@ import SignUp from './components/signUp';
 import Header from './components/header'
 import {getAuth, onAuthStateChanged} from 'firebase/auth'
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCvghWmEKLvML0aGufsmFmWTELxgs03aSA",
+    authDomain: "hhcodingtest.firebaseapp.com",
+    projectId: "hhcodingtest",
+    storageBucket: "hhcodingtest.appspot.com",
+    messagingSenderId: "770679417670",
+    appId: "1:770679417670:web:39c531cb45a7495d7e1347",
+    measurementId: "G-3BBYB7S06L"
+  };
+  
+
 require('dotenv').config();
 function App() {
     const [user, setUser] = useState('');
